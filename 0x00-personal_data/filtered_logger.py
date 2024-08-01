2f49e7e09fc81d7e214d02d228a57a114578f8b7
hellena254 <hellenah243@gmail.com>
2024-08-01 02:16:24 -0700

    modify the format log by implementing the format method to filter incoming logs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,21 +8,6 @@ import logging
 from typing import List
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
-
-
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
     Use a regex to replace occurrences of certain field values.
@@ -37,3 +22,34 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     """
     p = '|'.join(f"{field}=[^{separator}]+" for field in fields)
     return re.sub(p, lambda m: f"{m.group().split('=')[0]}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Initialize the formatter
+        Args:
+            fields: field to redact
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the log record, filtering specified fields.
+
+        Args:
+            record: The log record to format.
+
+        Return:
+            str: The formatted and obfuscated log record
+        """
+        initial_mess = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, initial_mess, self.SEPARATOR)
