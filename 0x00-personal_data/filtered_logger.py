072683214d2baa71b1085f75e1bde46fdc20f152
hellena254 <hellenah243@gmail.com>
2024-08-01 01:55:21 -0700

    modify log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,23 @@ REGEX-ING of logs
 """
 
 import re
+import logging
 from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
@@ -12,10 +28,10 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     Use a regex to replace occurrences of certain field values.
     Args:
         fields: The fields to obfuscate
-	reduction: The string to replace the field values with.
-	message: The log message
-	separator: The character that separates fields in the logs
-    
+        reduction: The string to replace the field values with.
+        message: The log message
+        separator: The character that separates fields in the logs
+
     Return:
         The log message obfuscated
     """
