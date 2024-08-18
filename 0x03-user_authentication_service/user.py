8a7dd031b13070580395fde40b313ad97c4e235f
hellena254 <hellenah243@gmail.com>
2024-08-18 06:10:14 -0700

    add file user that contains the SQLAlchemy model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+User model for SQLAlchemy
+"""
+
+from sqlalchemy import Colum, Integer, String, create_engine
+fron sqlalchemy.ext.declarative import declarative_base
+
+Base = dclarative_base()
+
+
+class User(Base):
+    """User model for the users table"""
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(string, nullable=False)
+    hashed_password = Column(string, nullable=False)
+    session_id = Column(string, nullable=True)
+    reset_token = Column(string, nullable=True)
