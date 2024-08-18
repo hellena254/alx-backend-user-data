1fa4b599caf823a3a9412bcebd823a0c804dfbce
hellena254 <hellenah243@gmail.com>
2024-08-18 07:08:00 -0700

    modify the db file

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,42 +1,53 @@
 #!/usr/bin/env python3
-"""
-DB module
-"""
+"""DB module for managing database operations related to users."""
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
 
 class DB:
-    """DB Class for interacting with the database."""
-    self._engine = create_engine("sqlite:///a.db", echo=True)
-    Base.metadata.drop_all(self._engine)
-    Base.metadata.create_all(self._engine)
-    self.__session = None
+    """DB class to handle database interactions for user management."""
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance with a SQLite database and set up tables.
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
 
     @property
     def _session(self) -> Session:
-        """The session object"""
+        """Memoized session object.
+
+        Returns:
+            Session: The current SQLAlchemy session.
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-    """Add a new user to the database
-    Args:
-         email
-         hashed_password
-    Returns:
-        User, the created user
-    """
-    new_user = User(email=email, hashed_password=hashed_password)
-    self._session.add(new_user)
-    self._session.commit()
-    return new_user
+        """Add a new user to the database.
+
+        Args:
+            email (str): The email of the new user.
+            hashed_password (str): The hashed password of the new user.
+
+        Returns:
+            User: The created User object.
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
 
     def find_user_by(self, **kwargs) -> User:
         """Find a user by specific attributes.
@@ -77,3 +88,4 @@ class DB:
 
         self._session.commit()
         return None
+
